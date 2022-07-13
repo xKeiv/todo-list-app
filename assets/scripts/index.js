cfb65285decfa97c1229053fda507dbd49a97b9f
xKavai <jak.wisn99@gmail.com>
2022-07-13 22:00:35 +0200

    Creating loop and forEach for user's deleting button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,12 +3,23 @@
 
 const addForm = document.querySelector('.mynavbar');
 const clearAll = document.getElementById('clrAll');
-const clearUser1 = document.getElementById('clUser1');
-const clearUser2 = document.getElementById('clUser2');
-const clearUser3 = document.getElementById('clUser3');
-const clearUser4 = document.getElementById('clUser4');
+//const clearUser1 = document.getElementById('clUser1');
+//const clearUser2 = document.getElementById('clUser2');
+//const clearUser3 = document.getElementById('clUser3');
+//const clearUser4 = document.getElementById('clUser4');
 
 const clearUser = document.querySelectorAll('.clUser')
+
+clearUser.forEach((clrButton) => {
+
+ clrButton.addEventListener('click', () =>
+    {
+        const parent = clrButton.closest('.appTaskArea');
+        const tasks = parent.querySelectorAll('.task')
+        tasks.forEach((task) => task.remove())
+    })
+}
+);
 
 const removeTask = (event) => {
     event.target.closest('.task').remove();
@@ -83,34 +94,34 @@ clearAll.addEventListener (
     }
 );
 
-clearUser1.addEventListener (
-    'click', () => {
-        const tasks = document.querySelectorAll('#user1 .task, #complUser1 .task');
+// clUser1.addEventListener (
+//     'click', () => {
+//         const tasks = document.querySelectorAll('#user1 .task, #complUser1 .task');
 
-        tasks.forEach((task) => task.remove());
-    }
-);
+//         tasks.forEach((task) => task.remove());
+//     }
+// );
 
-clearUser2.addEventListener (
-    'click', () => {
-        const tasks = document.querySelectorAll('#user2 .task, #complUser2 .task');
+// clearUser2.addEventListener (
+//     'click', () => {
+//         const tasks = document.querySelectorAll('#user2 .task, #complUser2 .task');
 
-        tasks.forEach((task) => task.remove());
-    }
-);
+//         tasks.forEach((task) => task.remove());
+//     }
+// );
 
-clearUser3.addEventListener (
-    'click', () => {
-        const tasks = document.querySelectorAll('#user3 .task, #complUser3 .task');
+// clearUser3.addEventListener (
+//     'click', () => {
+//         const tasks = document.querySelectorAll('#user3 .task, #complUser3 .task');
 
-        tasks.forEach((task) => task.remove());
-    }
-);
+//         tasks.forEach((task) => task.remove());
+//     }
+// );
 
-clearUser4.addEventListener (
-    'click', () => {
-        const tasks = document.querySelectorAll('#user4 .task, #complUser4 .task');
+// clearUser4.addEventListener (
+//     'click', () => {
+//         const tasks = document.querySelectorAll('#user4 .task, #complUser4 .task');
 
-        tasks.forEach((task) => task.remove());
-    }
-);
+//         tasks.forEach((task) => task.remove());
+//     }
+// );
